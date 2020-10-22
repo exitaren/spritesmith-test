f8498dddd0b79e5e8d0072d20612e4912a15422b
SINSA <exitaren@bigpi.co>
2020-10-22 21:33:56 +0900

    sprite smith test 1차

---
+++ vue.config.js
@@ -0,0 +1,49 @@
+var SpriteLight = require('webpack-spritesmith');
+var SpriteDark = require('webpack-spritesmith');
+var path = require('path');
+
+module.exports = {
+  configureWebpack: config => {
+    config.module.rules[
+        {
+          test: /\.(png|jpe?g|gif|svg)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+              publicPath: '../images/',
+            }
+          }
+        }
+      ]
+    config.plugins.push(
+      new SpriteLight({
+        src: {
+          cwd: path.resolve(__dirname, 'src', 'assets', 'images-light'),
+          glob: '*.png'
+        },
+        target: {
+          image: path.resolve(__dirname, 'src/spritesmith-generated/sprite-light.png'),
+          css: path.resolve(__dirname, 'src/spritesmith-generated/sprite-light.scss')
+        },
+        apiOptions: {
+          cssImageRef: "../spritesmith-generated/sprite-light.png"
+        }
+      }),
+      new SpriteDark({
+        src: {
+          cwd: path.resolve(__dirname, 'src', 'assets', 'images-dark'),
+          glob: '*.png'
+        },
+        target: {
+          image: path.resolve(__dirname, 'src/spritesmith-generated/sprite-dark.png'),
+          css: path.resolve(__dirname, 'src/spritesmith-generated/sprite-dark.scss')
+        },
+        apiOptions: {
+          cssImageRef: "../spritesmith-generated/sprite-dark.png"
+        }
+      })
+    )
+  }
+}
